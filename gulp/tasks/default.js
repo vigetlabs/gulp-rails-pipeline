bd2311eaf338e5e262fd2f949ae6089bc54d331d
Dan Tello <dan.tello@gmail.com>
2015-03-26 10:15:17 -0400

    Add images and iconFont to default task
    Fixes #15

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,3 +1,3 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['images', 'iconFont', 'sass', 'watch']);
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -10,5 +10,6 @@ var watch  = require('gulp-watch');
 gulp.task('watch', ['watchify','browserSync'], function(callback) {
   watch(config.sass.src, function() { gulp.start('sass'); });
   watch(config.images.src, function() { gulp.start('images'); });
+  watch(config.iconFont.src, function() { gulp.start('iconFont'); });
   // Watchify will watch and recompile our JS, so no need to gulp.watch it
 });
