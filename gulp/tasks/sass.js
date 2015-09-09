46c53fcb65f8c6ffc5e4a842ff6e9027c3ccdc65
Matt Jones <matt.jones@dxydoes.com>
2015-09-09 17:41:13 -0400

    write source maps after autoprefixer has run
    Addresses https://github.com/floridoo/gulp-sourcemaps/issues/60

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -11,8 +11,8 @@ gulp.task('sass', function () {
     .pipe(sourcemaps.init())
     .pipe(sass(config.settings))
     .on('error', handleErrors)
-    .pipe(sourcemaps.write())
     .pipe(autoprefixer({ browsers: ['last 2 version'] }))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.dest))
     .pipe(browserSync.reload({stream:true}));
 });
